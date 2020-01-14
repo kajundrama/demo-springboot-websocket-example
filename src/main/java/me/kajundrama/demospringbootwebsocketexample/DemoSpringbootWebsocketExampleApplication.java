458b57510f125ba0dca0a093ab3979de2246925c
leejunhwi <kajundrama@gmail.com>
2020-01-14 23:02:46 +0900

    Initial commit

---
+++ src/main/java/me/kajundrama/demospringbootwebsocketexample/DemoSpringbootWebsocketExampleApplication.java
@@ -0,0 +1,13 @@
+package me.kajundrama.demospringbootwebsocketexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootWebsocketExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootWebsocketExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kajundrama/demospringbootwebsocketexample/DemoSpringbootWebsocketExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kajundrama.demospringbootwebsocketexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootWebsocketExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
